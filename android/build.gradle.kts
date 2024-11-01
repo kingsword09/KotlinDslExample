53fedee5974a52cbc34694f55583e2c4fd84fb26
Kingsword <kingsword09@gmail.com>
2024-11-01 15:06:59 +0800

    :bug: fix third-party read config error

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     id("com.facebook.react.rootproject")
 }
 
+// Since some third-party libraries directly use the Android app configuration configured here, add these configurations
+extra["buildToolsVersion"] = libs.versions.buildToolsVersion.get()
+extra["minSdkVersion"] = libs.versions.minSdkVersion.get().toInt()
+extra["compileSdkVersion"] = libs.versions.compileSdkVersion.get().toInt()
+extra["targetSdkVersion"] = libs.versions.targetSdkVersion.get().toInt()
+extra["ndkVersion"] = libs.versions.ndkVersion.get()
+
 buildscript {
     repositories {
         google()
